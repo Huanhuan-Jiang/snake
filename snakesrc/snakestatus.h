d25432de9b62d546e35977c20a4125acbb8f6e77
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-05-18 22:01:42 +0800

    Test

--- snakesrc/snakestatus.h
+++ snakesrc/snakestatus.h
@@ -8,24 +8,25 @@ namespace snakestatus {
 enum class Direction { UP, DOWN, RIGHT, LEFT };
 // definition of class Snake
 class Snake {
- private:
-  std::vector<std::pair<int, int>> snakeBody;
-  int head_X;
-  int head_Y;
-  Direction headDir;
+   private:
+    std::vector<std::pair<int, int>> snakeBody;
+    int head_X;
+    int head_Y;
+    Direction headDir;
 
- public:
-  // Constructor
-  Snake();
-  Snake(std::vector<std::pair<int, int>> initialBody, Direction headDirection);
+   public:
+    // Constructor
+    Snake();
+    Snake(std::vector<std::pair<int, int>> initialBody,
+          Direction headDirection);
 
-  // Get the size of snake body
-  int size();
-  // Get the body of snake
-  std::vector<std::pair<int, int>> getBody();
-  // Move the snake one step further based on its head's direction
-  void move();
-  void eatFood(std::pair<int, int>);
-  Direction getDirection();
-};
+    // Get the size of snake body
+    int size();
+    // Get the body of snake
+    std::vector<std::pair<int, int>> getBody();
+    // Move the snake one step further based on its head's direction
+    void move();
+    void eatFood(std::pair<int, int>);
+    Direction getDirection();
+  };
 }  // namespace snakestatus
