110bcd067897cfa3b2606b4b68d5f192b32fc6ff
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-05-18 19:55:49 +0800

    Add clang-format and clang-tidy

--- tests/test_snake.cpp
+++ tests/test_snake.cpp
@@ -14,8 +14,8 @@ class SnakeTest : public ::testing::Test {
   void SetUp() override {
     body1_0 = {{20, 30}};  // one unit long
     body9_4 = {{20, 30}, {19, 30}, {19, 29}, {18, 29}, {17, 29},
-               {17, 28}, {17, 27}, {16, 27}, {15, 27}};  // nine units long with
-                                                         // 4 turns
+               {17, 28}, {17, 27}, {16, 27}, {15, 27}};  // nine units long
+                                                         // with 4 turns
   }
 
   std::vector<std::pair<int, int>> body1_0;
