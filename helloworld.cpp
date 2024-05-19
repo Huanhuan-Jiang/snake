68002ec90bcc284f735f383d77123a347cf96035
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-05-19 20:49:21 +0800

    Introduce errors to hellloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,3 +1,7 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World!" << std::endl; }
+int main() { 
+    std::cout << "Hello World!" << std::endl;
+
+    int x;
+    int sum = x + 2; } // introduce errors
