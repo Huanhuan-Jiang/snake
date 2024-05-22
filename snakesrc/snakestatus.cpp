2b2dce7d2793e80c2453823a5fea0c1e66157165
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-05-22 18:49:26 +0800

    Update format

--- snakesrc/snakestatus.cpp
+++ snakesrc/snakestatus.cpp
@@ -1,7 +1,7 @@
-#include <utility>
-#include <list>
-
 #include "snakestatus.h"
+
+#include <list>
+#include <utility>
 
 namespace snakestatus {
 
@@ -9,4 +9,4 @@ Snake::Snake(std::list<std::pair<int, int>> initial_body,
              Direction head_direction)
     : snake_body_(initial_body), head_dir_(head_direction) {}
 
-}
+}  // namespace snakestatus
--- snakesrc/snakestatus.h
+++ snakesrc/snakestatus.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <utility>
 #include <list>
+#include <utility>
 
 namespace snakestatus {
 
@@ -15,14 +15,11 @@ class Snake {
   Direction head_dir_;
 
  public:
-  Snake(std::list<std::pair<int, int>> initial_body,
-        Direction head_direction);
+  Snake(std::list<std::pair<int, int>> initial_body, Direction head_direction);
 
   std::size_t size() const noexcept { return snake_body_.size(); }
 
-  const std::list<std::pair<int, int>>& getBody() const {
-    return snake_body_;
-  }
+  const std::list<std::pair<int, int>>& getBody() const { return snake_body_; }
 
   const Direction& getDirection() const { return head_dir_; };
 };
