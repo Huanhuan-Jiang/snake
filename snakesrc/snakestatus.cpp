9d7d1b7fbc9d83810fd457c23b9e5ea70b7f51ac
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-05-26 08:31:16 +0800

    Apply std::move() to ctor

--- snakesrc/snakestatus.cpp
+++ snakesrc/snakestatus.cpp
@@ -10,7 +10,7 @@ namespace snakestatus {
 
 Snake::Snake(std::list<std::pair<int, int>> initial_body,
              Direction head_direction)
-    : snake_body_(initial_body), head_dir_(head_direction) {
+    : snake_body_(std::move(initial_body)), head_dir_(head_direction) {
   // Check if the snake body is coninuous and alive
   auto prev_it = snake_body_.begin();
   std::set<std::pair<int, int>> unique_elements;
