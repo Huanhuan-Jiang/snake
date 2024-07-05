7609705ed6c379554425a2a502df8ea1be1651ac
huanhuan-jiang <jhuanhustc@gmail.com>
2024-07-05 15:24:01 -0700

    Add submodule SDL and create try.cpp

---
+++ gamesrc/try.cpp
@@ -0,0 +1,34 @@
+#include <SDL3/SDL.h>
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL3 window",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(30000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    //SDL_Quit();
+    return 0;
+}
